4e1f42a1fa1f2b1eb3cecb6ee3043bd3a257d119
CoderRx001 <carrillo964@gmail.com>
2017-05-10 18:22:25 -0500

    update

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,54 @@
 class RecipesController < ApplicationController
   def one_recipe_action
     @recipe = Recipe.first
-    render 'one_recipe_view.html.erb'
-    
+    render 'one_recipe_view.html.erb' 
+  end
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    recipe_id = params[:id]
+    @recipe = Recipe.find_by(id: recipe_id)
+  end
+
+  def new
+     
+  end
+  def create
+    recipe = Recipe.new(
+                        title: params[:title],
+                        chef:  params[:chef],
+                        ingredients: params[:ingredients],
+                        directions: params[:directions]
+                         )
+    recipe.save
+    # render 'create.html.erb'
+    flash[:success] = "Recipe Successfully Created"
+    redirect_to "/recipes/#{ recipe.id }"
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  def update
+    recipe = Recipe.find(params[:id])
+    recipe.assign_attributes(
+                        title: params[:title],
+                        chef:  params[:chef],
+                        ingredients: params[:ingredients],
+                        description: params[:directions]
+                         )
+    recipe.save
+    flash[:success] = "Recipe Successfully Created"
+    redirect_to "/recipes/#{ recipe.id }"
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    flash[:warning] = "Recipe Destroyed"
+    redirect_to "/"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/one_recipe_url' => 'recipes#one_recipe_action'
+
+  get '/' => 'recipes#index'
+
+  get '/recipes' => 'recipes#index'
+
+  get '/recipes/new' => 'recipes#new' #these 2 are pairs
+  post '/recipes' => 'recipes#create'
+
+  get '/recipes/:id' => 'recipes#show'
+
+  get '/recipes/:id/edit' => 'recipes#edit' #these 2 are pairs
+  patch '/recipes/:id' => 'recipes#update'
+
+  delete '/recipes/:id' => 'recipes#destroy'
 end
